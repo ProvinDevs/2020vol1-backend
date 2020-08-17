7aafdf4fa9f9e2a38e417aacce34f41b5dbce1a0
isso0424 <isso04240129@gmail.com>
2020-08-17 15:36:26 +0900

    [Fix]Change port to constant value

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,9 @@
 use warp::Filter;
 
+const PORT: u16 = 3030;
+
 pub async fn serve() {
     let routes = warp::any().map(|| "UNCHI");
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(([127, 0, 0, 1], PORT)).await;
 }
