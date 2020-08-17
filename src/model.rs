cd34248f76054736079c6f148e33a3e98ed3deba
isso0424 <isso04240129@gmail.com>
2020-08-18 03:08:59 +0900

    [Add]Create new function for Class

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,11 @@
+extern crate rand;
+use crate::db::{Database, DatabaseError};
 use chrono::{DateTime, Utc};
+use rand::seq::SliceRandom;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+
+const SEED_STR: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
 pub struct ClassID(pub Uuid);
@@ -23,6 +28,50 @@ pub struct Class {
     pub pass_phrase: PassPhrase,
 
     pub files: Vec<File>,
+}
+
+impl Class {
+    pub async fn new(name: String, db: impl Database) -> Result<Self, DatabaseError> {
+        let id: Uuid;
+        loop {
+            let tmp = Uuid::new_v4();
+            if !db.check_existing_class_by_id(&ClassID(tmp)).await? {
+                id = tmp;
+                break;
+            }
+        }
+
+        let pass_phrase: PassPhrase;
+        loop {
+            let generated_phrase = Self::generate_pass_phrase(6);
+            if !db
+                .check_existing_class_by_pass_phrase(&PassPhrase(generated_phrase.clone()))
+                .await?
+            {
+                pass_phrase = PassPhrase(generated_phrase);
+                break;
+            }
+        }
+
+        Ok(Class {
+            id: ClassID(id),
+            pass_phrase: pass_phrase,
+            name: name,
+            files: vec![],
+        })
+    }
+
+    fn generate_pass_phrase(size: usize) -> String {
+        let mut rng = &mut rand::thread_rng();
+        String::from_utf8(
+            SEED_STR
+                .as_bytes()
+                .choose_multiple(&mut rng, size)
+                .cloned()
+                .collect(),
+        )
+        .unwrap()
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone)]
