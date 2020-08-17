3231b94fe1d93361b9c533f1b272fa7cfa49ee1a
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-08-17 23:54:27 +0900

    DBのエラー定義

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,7 @@
 use crate::model::*;
 use async_trait::async_trait;
 use serde::Serialize;
-use std::fmt::Debug;
+use thiserror::Error;
 
 #[derive(Serialize)]
 pub struct SimpleClassInfo {
@@ -14,16 +14,24 @@ pub struct SimpleClassInfo {
 
 #[async_trait]
 pub trait Database: Send {
-    type Error: Debug + Send;
+    async fn get_all_classes(&self) -> Result<Vec<SimpleClassInfo>, DatabaseError>;
+    async fn save_new_class(&mut self, _: &Class) -> Result<(), DatabaseError>;
+    async fn get_class_by_id(&self, class_id: &str) -> Result<Class, DatabaseError>;
+    async fn rename_class(&mut self, class_id: &str, new_name: &str) -> Result<(), DatabaseError>;
+    async fn delete_class(&mut self, class_id: &str) -> Result<Class, DatabaseError>;
 
-    async fn get_all_classes(&self) -> Result<Vec<SimpleClassInfo>, Self::Error>;
-    async fn save_new_class(&mut self, _: &Class) -> Result<(), Self::Error>;
-    async fn get_class_by_id(&self, class_id: &str) -> Result<Class, Self::Error>;
-    async fn rename_class(&mut self, class_id: &str, new_name: &str) -> Result<(), Self::Error>;
-    async fn delete_class(&mut self, class_id: &str) -> Result<Class, Self::Error>;
+    async fn get_files(&self, class_id: &str) -> Result<Vec<File>, DatabaseError>;
+    async fn add_new_file(&mut self, class_id: &str, file: File) -> Result<(), DatabaseError>;
+    async fn get_file_by_id(&self, file_id: &str) -> Result<File, DatabaseError>;
+    async fn delete_file(&mut self, file_id: &str) -> Result<File, DatabaseError>;
+}
 
-    async fn get_files(&self, class_id: &str) -> Result<Vec<File>, Self::Error>;
-    async fn add_new_file(&mut self, class_id: &str, file: File) -> Result<(), Self::Error>;
-    async fn get_file_by_id(&self, file_id: &str) -> Result<File, Self::Error>;
-    async fn delete_file(&mut self, file_id: &str) -> Result<File, Self::Error>;
+#[derive(Error, Debug)]
+#[non_exhaustive]
+pub enum DatabaseError {
+    #[error("specified class id not found")]
+    ClassNotFound,
+
+    #[error("specified file id not found")]
+    FileNotFound,
 }
