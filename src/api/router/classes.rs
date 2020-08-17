8766e133c6e07616355c6b939e517b2361d22427
Kota Isozaki <49902177+isso0424@users.noreply.github.com>
2020-08-18 03:10:36 +0900

    Classes/に対するルーターとハンドラを実装 (#29)

---
+++ src/api/router/classes.rs
@@ -0,0 +1,26 @@
+use super::{with_db, Synced};
+use crate::api::handler::classes;
+use crate::db::Database;
+use std::sync::Arc;
+use warp::Filter;
+
+pub fn classes<D>(
+    db: Synced<D>,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
+where
+    D: Database, // here?
+{
+    classes_list(Arc::clone(&db))
+}
+
+fn classes_list<D>(
+    db: Synced<D>,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
+where
+    D: Database, // here?
+{
+    warp::path!("classes")
+        .and(warp::get())
+        .and(with_db(db))
+        .and_then(classes::classes_list)
+}
