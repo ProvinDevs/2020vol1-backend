3365f9594986bba38b09e40d7eb52fd03ed41d7c
かわえもん <34652535+kawaemon@users.noreply.github.com>
2020-08-18 01:13:36 +0900

    そういえばDBトレイトのClassIDとかってStruct使わないとですね (#27)
    * use id struct for db trait
    
    * shut clippy up

--- src/db.rs
+++ src/db.rs
@@ -16,18 +16,23 @@ pub struct SimpleClassInfo {
 pub trait Database: Send {
     async fn get_all_classes(&self) -> Result<Vec<SimpleClassInfo>, DatabaseError>;
     async fn save_new_class(&mut self, _: &Class) -> Result<(), DatabaseError>;
-    async fn get_class_by_id(&self, class_id: &str) -> Result<Class, DatabaseError>;
-    async fn rename_class(&mut self, class_id: &str, new_name: &str) -> Result<(), DatabaseError>;
-    async fn delete_class(&mut self, class_id: &str) -> Result<Class, DatabaseError>;
+    async fn get_class_by_id(&self, class_id: &ClassID) -> Result<Class, DatabaseError>;
+    async fn rename_class(
+        &mut self,
+        class_id: &ClassID,
+        new_name: &str,
+    ) -> Result<(), DatabaseError>;
+    async fn delete_class(&mut self, class_id: &ClassID) -> Result<Class, DatabaseError>;
 
-    async fn get_files(&self, class_id: &str) -> Result<Vec<File>, DatabaseError>;
-    async fn add_new_file(&mut self, class_id: &str, file: File) -> Result<(), DatabaseError>;
-    async fn get_file_by_id(&self, file_id: &str) -> Result<File, DatabaseError>;
-    async fn delete_file(&mut self, file_id: &str) -> Result<File, DatabaseError>;
+    async fn get_files(&self, class_id: &ClassID) -> Result<Vec<File>, DatabaseError>;
+    async fn add_new_file(&mut self, class_id: &ClassID, file: File) -> Result<(), DatabaseError>;
+    async fn get_file_by_id(&self, file_id: &FileID) -> Result<File, DatabaseError>;
+    async fn delete_file(&mut self, file_id: &FileID) -> Result<File, DatabaseError>;
 }
 
 #[derive(Error, Debug)]
 #[non_exhaustive]
+#[allow(dead_code)]
 pub enum DatabaseError {
     #[error("specified class id not found")]
     ClassNotFound,
