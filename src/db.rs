0ea59c0e7f09afe1406b3a06357dc7f09e906437
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-08-17 17:18:05 +0900

    rename methods

--- src/db.rs
+++ src/db.rs
@@ -12,9 +12,9 @@ pub struct SimpleClassInfo {
 pub trait Database: Send {
     type Error: Debug + Send;
 
-    fn get_all(&self) -> Result<Vec<SimpleClassInfo>, Self::Error>;
+    fn get_all_classes(&self) -> Result<Vec<SimpleClassInfo>, Self::Error>;
     fn save_new_class(&mut self, _: &Class) -> Result<(), Self::Error>;
-    fn get_class(&self, class_id: &str) -> Result<Class, Self::Error>;
+    fn get_class_by_id(&self, class_id: &str) -> Result<Class, Self::Error>;
     fn rename_class(&mut self, class_id: &str, new_name: &str) -> Result<(), Self::Error>;
     fn delete_class(&mut self, class_id: &str) -> Result<Class, Self::Error>;
 
