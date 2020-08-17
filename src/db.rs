e8af499ff289beb4230e8e8d9524f17ae411627f
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-08-18 01:32:04 +0900

    Implement Memory DB

--- src/db.rs
+++ src/db.rs
@@ -1,3 +1,5 @@
+pub mod mem;
+
 use crate::model::*;
 use async_trait::async_trait;
 use serde::Serialize;
@@ -13,7 +15,7 @@ pub struct SimpleClassInfo {
 }
 
 #[async_trait]
-pub trait Database: Send {
+pub trait Database: Send + Sync {
     async fn get_all_classes(&self) -> Result<Vec<SimpleClassInfo>, DatabaseError>;
     async fn save_new_class(&mut self, _: &Class) -> Result<(), DatabaseError>;
     async fn get_class_by_id(&self, class_id: &ClassID) -> Result<Class, DatabaseError>;
@@ -25,7 +27,7 @@ pub trait Database: Send {
     async fn delete_class(&mut self, class_id: &ClassID) -> Result<Class, DatabaseError>;
 
     async fn get_files(&self, class_id: &ClassID) -> Result<Vec<File>, DatabaseError>;
-    async fn add_new_file(&mut self, class_id: &ClassID, file: File) -> Result<(), DatabaseError>;
+    async fn add_new_file(&mut self, class_id: &ClassID, file: &File) -> Result<(), DatabaseError>;
     async fn get_file_by_id(&self, file_id: &FileID) -> Result<File, DatabaseError>;
     async fn delete_file(&mut self, file_id: &FileID) -> Result<File, DatabaseError>;
 }
---
+++ src/db/mem.rs
@@ -0,0 +1,94 @@
+use crate::db::{Database, DatabaseError, SimpleClassInfo};
+use crate::model::*;
+use async_trait::async_trait;
+
+pub struct MemoryDB {
+    inner: Vec<Class>,
+}
+
+#[async_trait]
+impl Database for MemoryDB {
+    async fn get_all_classes(&self) -> Result<Vec<SimpleClassInfo>, DatabaseError> {
+        let infos = self
+            .inner
+            .iter()
+            .map(|c| SimpleClassInfo {
+                name: c.name.clone(),
+                id: c.id.clone(),
+                pass_phrase: c.pass_phrase.clone(),
+            })
+            .collect::<Vec<_>>();
+
+        Ok(infos)
+    }
+
+    async fn save_new_class(&mut self, c: &Class) -> Result<(), DatabaseError> {
+        self.inner.push(c.clone());
+        Ok(())
+    }
+
+    async fn get_class_by_id(&self, class_id: &ClassID) -> Result<Class, DatabaseError> {
+        self.inner
+            .iter()
+            .find(|c| c.id == *class_id)
+            .map_or_else(|| Err(DatabaseError::ClassNotFound), |c| Ok(c.clone()))
+    }
+
+    async fn rename_class(
+        &mut self,
+        class_id: &ClassID,
+        new_name: &str,
+    ) -> Result<(), DatabaseError> {
+        self.inner
+            .iter_mut()
+            .find(|c| c.id == *class_id)
+            .ok_or_else(|| DatabaseError::ClassNotFound)?
+            .name = new_name.to_string();
+
+        Ok(())
+    }
+
+    async fn delete_class(&mut self, class_id: &ClassID) -> Result<Class, DatabaseError> {
+        let index = self
+            .inner
+            .iter()
+            .position(|c| c.id == *class_id)
+            .ok_or_else(|| DatabaseError::ClassNotFound)?;
+
+        Ok(self.inner.remove(index))
+    }
+
+    async fn get_files(&self, class_id: &ClassID) -> Result<Vec<File>, DatabaseError> {
+        Ok(self.get_class_by_id(class_id).await?.files.clone())
+    }
+
+    async fn add_new_file(&mut self, class_id: &ClassID, file: &File) -> Result<(), DatabaseError> {
+        self.inner
+            .iter_mut()
+            .find(|c| c.id == *class_id)
+            .ok_or_else(|| DatabaseError::ClassNotFound)?
+            .files
+            .push(file.clone());
+
+        Ok(())
+    }
+
+    async fn get_file_by_id(&self, file_id: &FileID) -> Result<File, DatabaseError> {
+        self.inner
+            .iter()
+            .flat_map(|c| c.files.iter())
+            .find(|f| f.id == *file_id)
+            .ok_or(DatabaseError::FileNotFound)
+            .map(|f| f.clone())
+    }
+
+    async fn delete_file(&mut self, file_id: &FileID) -> Result<File, DatabaseError> {
+        for class in &mut self.inner {
+            if let Some(index) = class.files.iter().position(|f| f.id == *file_id) {
+                return Ok(class.files.remove(index));
+            }
+        }
+
+        Err(DatabaseError::FileNotFound)
+    }
+}
