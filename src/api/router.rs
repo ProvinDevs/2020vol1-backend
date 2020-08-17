2ca5af7dc5ca170a7809fd96d0715959eb16cac2
isso0424 <isso04240129@gmail.com>
2020-08-17 23:44:47 +0900

    [Add]Create with_db function

--- src/api/router.rs
+++ src/api/router.rs
@@ -1,1 +1,17 @@
+use std::sync::{Arc, Mutex};
+use warp::Filter;
+
+use crate::db::Database;
+
 mod classes;
+
+type Synced<T> = Arc<Mutex<T>>;
+
+fn with_db<D>(
+    db: Synced<D>,
+) -> impl Filter<Extract = (Synced<D>,), Error = std::convert::Infallible> + Clone
+where
+    D: Database, // here?
+{
+    warp::any().map(move || Arc::clone(&db))
+}
