a5f632dd55ddfe51b6e390d2b63e8252c7e29d9e
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-08-17 17:11:35 +0900

    define db trait and id structs

---
+++ src/db.rs
@@ -0,0 +1,26 @@
+use crate::model::*;
+use std::fmt::Debug;
+use uuid::Uuid;
+
+// todo: delete attr after use this
+#[allow(dead_code)]
+pub struct SimpleClassInfo {
+    name: String,
+    id: Uuid,
+    pass_phrase: String,
+}
+
+pub trait Database: Send {
+    type Error: Debug + Send;
+
+    fn get_all(&self) -> Result<Vec<SimpleClassInfo>, Self::Error>;
+    fn save_new_class(&mut self, _: &Class) -> Result<(), Self::Error>;
+    fn get_class(&self, class_id: &str) -> Result<Class, Self::Error>;
+    fn rename_class(&mut self, class_id: &str, new_name: &str) -> Result<(), Self::Error>;
+    fn delete_class(&mut self, class_id: &str) -> Result<Class, Self::Error>;
+
+    fn get_files(&self, class_id: &str) -> Result<Vec<File>, Self::Error>;
+    fn add_new_file(&mut self, class_id: &str, file: File) -> Result<(), Self::Error>;
+    fn get_file_by_id(&self, file_id: &str) -> Result<File, Self::Error>;
+    fn delete_file(&mut self, file_id: &str) -> Result<File, Self::Error>;
+}
