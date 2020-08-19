1cb02493503f3074d8cf9caea9c905124970ebec
isso0424 <isso04240129@gmail.com>
2020-08-19 22:20:32 +0900

    [Add]***done***

--- src/api/routes/class.rs
+++ src/api/routes/class.rs
@@ -1,7 +1,8 @@
-use super::{with_db, ApiDBError, IDParsingError};
+use super::{with_db, with_json_body, ApiDBError, IDParsingError};
 use crate::db::Database;
 use crate::model::ClassID;
 use crate::Synced;
+use serde::Deserialize;
 use std::str::FromStr;
 use std::sync::Arc;
 use warp::Filter;
@@ -9,7 +10,7 @@ use warp::Filter;
 pub(super) fn class(
     db: &Synced<impl Database>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    get(Arc::clone(db))
+    get(Arc::clone(db)).or(put(Arc::clone(db)))
 }
 
 fn get(
@@ -19,6 +20,16 @@ fn get(
         .and(warp::get())
         .and(with_db(db))
         .and_then(on_get)
+}
+
+fn put(
+    db: Synced<impl Database>,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("classes" / String)
+        .and(warp::put())
+        .and(with_db(db))
+        .and(with_json_body())
+        .and_then(on_put)
 }
 
 async fn on_get(
@@ -37,4 +48,28 @@ async fn on_get(
         .map_err(warp::reject::custom)?;
 
     Ok(warp::reply::json(&class))
+}
+
+async fn on_put(
+    raw_id: String,
+    db: Synced<impl Database>,
+    body: PutRequestBody,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let id = ClassID::from_str(raw_id.as_str())
+        .map_err(IDParsingError)
+        .map_err(warp::reject::custom)?;
+    let _ = db
+        .lock()
+        .await
+        .rename_class(&id, body.name.as_str())
+        .await
+        .map_err(ApiDBError)
+        .map_err(warp::reject::custom)?;
+
+    Ok(warp::http::StatusCode::NO_CONTENT)
+}
+
+#[derive(Deserialize)]
+struct PutRequestBody {
+    name: String,
 }
