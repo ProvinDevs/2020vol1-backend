7d2ed4c01d06486fabd9355c5c59770aa2483e4d
Kota Isozaki <49902177+isso0424@users.noreply.github.com>
2020-08-21 02:25:59 +0900

    resolve#7 (#43)
    Co-authored-by: kawaemon <34652535+kawaemon@users.noreply.github.com>

--- src/api/routes/class.rs
+++ src/api/routes/class.rs
@@ -1,7 +1,8 @@
-use super::{with_db, ApiDBError, IDParsingError};
+use super::{with_db, with_json_body, ApiDBError, IDParsingError};
 use crate::db::Database;
 use crate::model::ClassID;
 use crate::Synced;
+use serde::Deserialize;
 use std::str::FromStr;
 use std::sync::Arc;
 use warp::Filter;
@@ -9,7 +10,7 @@ use warp::Filter;
 pub(super) fn class(
     db: &Synced<impl Database>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    get(Arc::clone(db))
+    get(Arc::clone(db)).or(put(Arc::clone(db)))
 }
 
 fn get(
@@ -28,6 +29,7 @@ async fn on_get(
     let id = ClassID::from_str(raw_id.as_str())
         .map_err(IDParsingError)
         .map_err(warp::reject::custom)?;
+
     let class = db
         .lock()
         .await
@@ -37,4 +39,38 @@ async fn on_get(
         .map_err(warp::reject::custom)?;
 
     Ok(warp::reply::json(&class))
+}
+
+fn put(
+    db: Synced<impl Database>,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("classes" / String)
+        .and(warp::put())
+        .and(with_db(db))
+        .and(with_json_body())
+        .and_then(on_put)
+}
+
+#[derive(Deserialize)]
+struct PutRequestBody {
+    name: String,
+}
+
+async fn on_put(
+    raw_id: String,
+    db: Synced<impl Database>,
+    body: PutRequestBody,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let id = ClassID::from_str(raw_id.as_str())
+        .map_err(IDParsingError)
+        .map_err(warp::reject::custom)?;
+
+    db.lock()
+        .await
+        .rename_class(&id, body.name.as_str())
+        .await
+        .map_err(ApiDBError)
+        .map_err(warp::reject::custom)?;
+
+    Ok(warp::http::StatusCode::NO_CONTENT)
 }
