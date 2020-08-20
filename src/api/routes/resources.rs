37817a0c9ccbe8694d625c13820970f1f5faea83
Kota Isozaki <49902177+isso0424@users.noreply.github.com>
2020-08-21 02:28:02 +0900

    resolve#9 (#44)
    Co-authored-by: kawaemon <34652535+kawaemon@users.noreply.github.com>

--- src/api/routes/resources.rs
+++ src/api/routes/resources.rs
@@ -1,7 +1,9 @@
-use super::{with_db, ApiDBError, IDParsingError};
+use super::{with_db, with_json_body, ApiDBError, IDParsingError};
 use crate::db::Database;
-use crate::model::ClassID;
+use crate::model::{ArMarkerID, ClassID, File};
 use crate::Synced;
+use chrono::{TimeZone, Utc};
+use serde::Deserialize;
 use std::str::FromStr;
 use std::sync::Arc;
 use warp::Filter;
@@ -9,13 +11,13 @@ use warp::Filter;
 pub(super) fn resources(
     db: &Synced<impl Database>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    get(Arc::clone(db))
+    get(Arc::clone(db)).or(post(Arc::clone(db)))
 }
 
 fn get(
     db: Synced<impl Database>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path!("classes" / String / "resources")
+    warp::path!("classes" / String / "files")
         .and(warp::get())
         .and(with_db(db))
         .and_then(on_get)
@@ -38,4 +40,57 @@ async fn on_get(
         .map_err(warp::reject::custom)?;
 
     Ok(warp::reply::json(&resources))
+}
+
+fn post(
+    db: Synced<impl Database>,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("classes" / String / "files")
+        .and(warp::post())
+        .and(with_db(db))
+        .and(with_json_body())
+        .and_then(on_post)
+}
+
+#[derive(Deserialize)]
+struct PostRequestBody {
+    #[serde(rename = "markerID")]
+    marker_id: String,
+    #[serde(rename = "resourceInfo")]
+    resource_info: ResourceRequestBody,
+}
+
+#[derive(Deserialize)]
+struct ResourceRequestBody {
+    #[serde(rename = "fileName")]
+    file_name: String,
+    #[serde(rename = "createdAt")]
+    created_at: i64,
+}
+
+async fn on_post(
+    raw_id: String,
+    db: Synced<impl Database>,
+    body: PostRequestBody,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let class_id = ClassID::from_str(raw_id.as_str())
+        .map_err(IDParsingError)
+        .map_err(warp::reject::custom)?;
+
+    let marker_id = ArMarkerID(body.marker_id);
+    let created_at = Utc.timestamp(body.resource_info.created_at, 0);
+
+    let file = File::new(&db, marker_id, body.resource_info.file_name, created_at)
+        .await
+        .map_err(ApiDBError)
+        .map_err(warp::reject::custom)?;
+
+    db.lock()
+        .await
+        .add_new_file(&class_id, &file)
+        .await
+        .map_err(ApiDBError)
+        .map_err(warp::reject::custom)?;
+
+    Ok(warp::reply::json(&file))
 }
